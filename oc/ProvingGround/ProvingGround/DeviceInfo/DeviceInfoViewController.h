5ed4bd96e2614e27ee0a9190b0854a15cb89d7db
Mark Chen <chenjian345@gmail.com>
2019-07-19 15:51:07 +0800

    Move the resource directory and add DeviceInfoViewController

---
+++ oc/ProvingGround/ProvingGround/DeviceInfo/DeviceInfoViewController.h
@@ -0,0 +1,17 @@
+//
+//  DeviceInfoViewController.h
+//  ProvingGround
+//
+//  Created by Mark on 2019/7/19.
+//  Copyright © 2019 markcj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DeviceInfoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ oc/ProvingGround/ProvingGround/DeviceInfo/DeviceInfoViewController.m
@@ -0,0 +1,32 @@
+//
+//  DeviceInfoViewController.m
+//  ProvingGround
+//
+//  Created by Mark on 2019/7/19.
+//  Copyright © 2019 markcj. All rights reserved.
+//
+
+#import "DeviceInfoViewController.h"
+
+@interface DeviceInfoViewController ()
+
+@end
+
+@implementation DeviceInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
